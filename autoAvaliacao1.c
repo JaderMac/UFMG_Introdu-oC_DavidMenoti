c00b3b7be4a53b66aa0db14ab36b7374e9b0ff9b
JaderMach <jadermachado@live.com>
2024-03-11 17:54:04 -0300

    Autoavaliação 1 - página 14!

---
+++ autoAvaliacao1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    printf("%d",x);
+    return(0);
+}
+// o programa cria uma variavel que recebe um valor digitado pelo usuário e depois apresenta ele na tela.

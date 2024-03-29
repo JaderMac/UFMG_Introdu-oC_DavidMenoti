6234785a47aebc95221f2ab8935b6ca4f6a0f5e2
JaderMach <jadermachado@live.com>
2024-03-29 12:14:59 -0300

    Auto avaliacao 11 - pag 55

---
+++ autoAvaliacao11.c
@@ -0,0 +1,38 @@
+/**
+Refaça o programa da seção anterior (4.3). Use o comando do-while para fechar o loop.
+*/
+#include <stdio.h>
+#include <string.h>
+
+int retornaTamanhoString(char *string){
+    int total=0;
+    while( string[total] != '\0')
+        total++;
+    return total;
+}
+
+main(){
+
+    int tamanhoPalavra = 0, iteracao;
+    char stringDigitada[25];
+
+    printf("Digite uma palavra : ");
+    scanf("%s", stringDigitada);
+
+    tamanhoPalavra = retornaTamanhoString(stringDigitada);
+
+    //for(int i = tamanhoPalavra; i >= 0; i--){
+    //    printf("%c\n", stringDigitada[i]);
+    //} 
+    // iteracao = tamanhoPalavra;
+    // while(iteracao >= 0){ 
+    //   printf("%c\n", stringDigitada[iteracao]);
+    //   iteracao--;
+    //}
+    iteracao = tamanhoPalavra;
+    do{
+        printf("%c\n", stringDigitada[iteracao]);
+        iteracao--;
+    }while(iteracao>=0);
+
+}

8e4a6024b3a1bacbf7b77451b21b55151392d851
JaderMach <jadermachado@live.com>
2024-04-12 22:40:29 -0300

    Auto avaliacao 18 - pag 79

---
+++ autoAvaliacao18.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void StrCpy (char *destino,char *origem)
+{
+    while (*origem){
+        *destino=*origem;
+        origem++;
+        destino++;
+    }
+    *destino='\0';
+}
+
+char StrCat(char *destino, char *origem){
+    *destino += *origem;
+}
+
+
+int StrLen(char *str){
+    int total=0;
+    while( str[total] != '\0')
+        total++;
+    return total;
+}
+
+int main ()
+{   
+    char nome[20] = "Jader";
+    char sobrenome[30] = "Machado";
+
+    printf("Candidato : %s", StrCat(nome, sobrenome));
+    return(0);
+}

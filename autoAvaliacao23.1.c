3bf510a426ff719becea1d89cdc222592e04f085
JaderMach <jadermachado@live.com>
2024-04-18 22:56:22 -0300

    Autoavaliação 23 - pag 94

---
+++ autoAvaliacao23.1.c
@@ -0,0 +1,7 @@
+int EPar (int a)
+{
+    if (a%2) /* Verifica se a e divisivel por dois */
+    return 0;
+    else
+    return 1;
+}
---
+++ autoAvaliacao23.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "autoAvaliacao23.1.h"
+void main ()
+{
+    int num;
+    printf ("Entre com numero: ");
+    scanf ("%d",&num);
+    if (EPar(num))
+    printf ("\n\nO numero e par.\n");
+    else
+    printf ("\n\nO numero e impar.\n");
+}
